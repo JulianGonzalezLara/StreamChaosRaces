ad24dbc5718161d799079b8559d66b06519e14a5
JulianGonzalezLara <juliangl803@gmail.com>
2022-05-31 20:43:01 +0200

    Spawn Twitch Coches

--- StreamChaosRaces/Assets/Karting/Scripts/Utilities/CineMachineTargeteer.cs
+++ StreamChaosRaces/Assets/Karting/Scripts/Utilities/CineMachineTargeteer.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace KartGame.Utilities
@@ -10,7 +11,7 @@ namespace KartGame.Utilities
     public class CineMachineTargeteer : MonoBehaviour
     {
         [Tooltip("What gameObjects do we want to look at?")]
-        public Transform[] Targets;
+        public List<Transform> Targets;
         [Tooltip("Which key allows us to switch targets? By default, we use the Spacebar.")]
         public KeyCode CyclingKey = KeyCode.Space;
 
@@ -18,7 +19,7 @@ namespace KartGame.Utilities
         int m_Index;
 
         void Start()
-        {
+        {            
             m_VirtualCam = GetComponent<CinemachineVirtualCamera>();
             m_Index = 0;
         }
@@ -27,7 +28,7 @@ namespace KartGame.Utilities
         {
             if (Input.GetKeyDown(CyclingKey))
             {
-                m_Index             = (m_Index + 1) % Targets.Length;
+                m_Index             = (m_Index + 1) % Targets.Count;
                 Transform target    = Targets[m_Index];
                 m_VirtualCam.Follow = m_VirtualCam.LookAt = target;
 
@@ -35,6 +36,15 @@ namespace KartGame.Utilities
                 // By pinging the gameObject we can easily find the gameObject in the Hierarchy view.
                 UnityEditor.EditorGUIUtility.PingObject(m_VirtualCam.Follow);
 #endif
+            }
+        }
+
+        public void RefrescarCoches()
+        {
+            Targets.Clear();
+            foreach (GameObject i in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                Targets.Add(i.transform);
             }
         }
     }
---
+++ StreamChaosRaces/Assets/Scripts/RaceManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using TwitchChatConnect.Client;
+using TwitchChatConnect.Config;
+using TwitchChatConnect.Data;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+    private static string START_COMMAND = "!play";
+
+    public string username;
+    public string userToken;
+    public string channelName;
+
+    public List<Transform> Spawns;
+    public GameObject playerPrefab;
+    private GameObject player;
+
+    private int contadorPlayers = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        TwitchChatClient.instance.Init(new TwitchConnectConfig(username, userToken, channelName),() =>
+        {
+            TwitchChatClient.instance.onChatMessageReceived += OnChatMessageReceived;
+            TwitchChatClient.instance.onChatCommandReceived += OnChatCommandReceived;
+            TwitchChatClient.instance.onChatRewardReceived += OnChatRewardReceived;
+        },
+                message =>
+                {
+                    // Error when initializing.
+                    Debug.LogError(message);
+                });
+
+        foreach (GameObject i in GameObject.FindGameObjectsWithTag("Spawn"))
+        {
+            Spawns.Add(i.transform);
+        }
+    }
+
+    private void OnChatCommandReceived(TwitchChatCommand chatCommand)
+    {
+        if (chatCommand.Command == START_COMMAND)
+        {
+            player = Instantiate(playerPrefab, Spawns[contadorPlayers].transform.position, Spawns[contadorPlayers].transform.rotation);
+            GameObject.FindGameObjectWithTag("CmCam").GetComponent<KartGame.Utilities.CineMachineTargeteer>().RefrescarCoches();
+            contadorPlayers++;
+        }
+        else
+        {
+            Debug.Log($"Unknown Command received: {chatCommand.Command}");
+        }        
+    }
+
+    private void OnChatRewardReceived(TwitchChatReward chatReward)
+    {
+    }
+
+    private void OnChatMessageReceived(TwitchChatMessage chatMessage)
+    {
+    }
+
+    private void OnMatchBegin()
+    {
+        TwitchChatClient.instance.SendChatMessage("A new game has started");
+    }
+}
